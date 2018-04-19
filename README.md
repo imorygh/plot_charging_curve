e1a75a0b65a647cf20c3410aa42447889b7fce55
Ray Ren <rl42>
2018-04-19 22:27:26 +0800

    re-init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# plot_charging_curvep
+# plot_charging_curve
 绘制充电曲线
--- plot_triaxis.py
+++ plot_triaxis.py
@@ -1,17 +1,18 @@
 from pylab import *
 
+
 def get_func_pic(dir):
     with open('{}/capacity'.format(dir)) as capacity,\
-            open('{}/current'.format(nudir as current,\
-            open('{}/voltage'.format(num)) as voltage:
-        cap = [int(i) for i in capacity]
-        cur = [-int(i) / 1000 for i in current]
-        vol = [int(i) / 1000 for i in voltage]
-    x = [i for i in range(1, 1 + len(cap))]
-    
-    fig, ax1 = subplots()
-    ax2 = ax1.twinx()
-    ax3 = ax2.twinx()
+            open('{}/current'.format(dir)) as current,\
+            open('{}/voltage'.format(dir)) as voltage:
+        cap=[int(i) for i in capacity]
+        cur=[-int(i) / 1000 for i in current]
+        vol=[int(i) / 1000 for i in voltage]
+    x=[i for i in range(1, 1 + len(cap))]
+
+    _, ax1=subplots()
+    ax2=ax1.twinx()
+    ax3=ax2.twinx()
     ax1.plot(x, cap, 'r')
     ax2.plot(x, cur, 'g')
     ax3.plot(x, vol, 'b')
@@ -19,9 +20,9 @@ def get_func_pic(dir):
     ax1.set_xlabel('time (s)', fontsize=16)
     ax1.set_ylim(0, 110)
     ax1.set_ylabel('capcity (%) ', fontsize=16, color='r')
-    ax2.set_ylim(-200,2350)
+    ax2.set_ylim(-200, 2350)
     ax2.set_ylabel('current (mA) ', fontsize=16, color='g')
-    ax3.set_ylim(3700,4250)
+    ax3.set_ylim(3700, 4250)
     ax3.set_ylabel('voltage (mV) ', fontsize=16, color='b')
     show()
 
